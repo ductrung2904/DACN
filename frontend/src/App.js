7e97a58da988ed173da564cdc6257082397a9791
Duc Trung <ductrung0429.acc@gmail.com>
2021-08-22 01:54:52 +0700

    Fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,10 @@ import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import Footer from './components/Footer';
 import ProductDetail from './pages/ProductDetail';
-import { ProductContextProvider } from './context/ProductContext';
 
 function App() {
   return (
-    <ProductContextProvider>
+    <div>
       <Header />
       <Router>
         <Switch>
@@ -18,7 +17,7 @@ function App() {
       </Router>
       <script src="./js/bootstrap.bundle.min.js"></script>
       <Footer />
-    </ProductContextProvider>
+    </div>
   );
 }
 
